d700d9cb9e480d7c3cd9603743de8692a17ec62e
Joel Kalos <joelkalos1@Joels-MacBook-Pro.local>
2015-02-07 16:36:07 -0500

    Added LeapMotion support to Kevin's stuff

---
+++ Kevin070451/Assets/LeapMotion/Leap_Boilerplate/scripts_for_example_scenes/NormalVectorChecker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class NormalVectorChecker : FingerCountChecker {
+	public HandController hand;
+
+	private TextMesh text;
+	// Use this for initialization
+	void Start () {
+		text = gameObject.GetComponent(typeof(TextMesh)) as TextMesh;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Normal Vector: " + hand.getNormal();
+	}
+}
