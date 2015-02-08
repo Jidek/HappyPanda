ac8f72b909baec7075d8022432a5fed5d7e2fbad
Joel Kalos <joelkalos1@Joels-MacBook-Pro.local>
2015-02-07 19:49:46 -0500

    Now flies, kind of

---
+++ Kevin070451/Assets/Scripts for Leap/LeapPlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeapPlayerController : MonoBehaviour {
+
+	public HandController palmPilot;
+
+	public float forceMagnitude;
+
+	private Vector3 lastForce = Vector3.zero;
+	private Vector3 currentForce, deltaForce;
+
+	void FixedUpdate () {
+		currentForce = Vector3.Scale (new Vector3 (forceMagnitude, forceMagnitude, forceMagnitude), palmPilot.getNormal ());
+		deltaForce = currentForce - lastForce;
+		Debug.Log("Normal Vector is: " + palmPilot.getNormal());
+		rigidbody.AddForce (deltaForce);//, ForceMode.Acceleration);
+	}
+}
