eb108471505a985c44f6786b9d8e060f2d5c0b5c
Joel Kalos <joelkalos1@Joels-MacBook-Pro.local>
2015-02-08 05:04:02 -0500

    mvp done

--- Kevin070451/Assets/Scripts for Leap/LeapPlayerController.cs
+++ Kevin070451/Assets/Scripts for Leap/LeapPlayerController.cs
@@ -4,21 +4,27 @@ using System.Collections;
 public class LeapPlayerController : MonoBehaviour {
 
 	public HandController palmPilot;
+	public RotationController playerController;
 
 	public float forceBaseMagnitude;
 	public float xMultiplier, yMultiplier, zMultiplier;
 
 	private Vector3 lastForce = Vector3.zero;
-	private Vector3 currentForce, deltaForce;
+	private Vector3 currentForce, deltaForce, inputForce;
 	private float forceMagnitude, height;
 
 	void FixedUpdate () {
 		height = palmPilot.getHeight ();
 		forceMagnitude = forceBaseMagnitude / ((height * height) + 1);
-		currentForce = Vector3.Scale (new Vector3 (xMultiplier * forceMagnitude, yMultiplier * forceMagnitude, zMultiplier * forceMagnitude), palmPilot.getNormal ());
+		inputForce = translate (palmPilot.getNormal (), playerController.changeInRotation ());
+		currentForce = Vector3.Scale (new Vector3 (xMultiplier * forceMagnitude, yMultiplier * forceMagnitude, zMultiplier * forceMagnitude), inputForce);
 		deltaForce = currentForce - lastForce;
 		Debug.Log("Normal Vector is: " + palmPilot.getNormal());
 		Debug.Log ("Height is: " + palmPilot.getHeight ());
 		rigidbody.AddForce (deltaForce);//, ForceMode.Acceleration);
+	}
+
+	private Vector3 translate(Vector3 normalVector, float angle){
+		return new Vector3 (normalVector.z * Mathf.Sin (angle) + normalVector.x * Mathf.Cos (angle), normalVector.y, normalVector.z * Mathf.Cos (angle) - normalVector.x * Mathf.Sin (angle));
 	}
 }
---
+++ Kevin070451/Assets/Scripts for Leap/RotationController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationController : MonoBehaviour {
+
+	public float rotationAmount = 1.5f;
+
+	private Quaternion initialRotation;
+	private Quaternion currentRotation;
+
+	void Start () {
+		initialRotation = transform.rotation;
+	}
+
+	void Update () {
+
+		if(Input.GetKey(KeyCode.A)){
+			Quaternion cRotation = transform.rotation;
+			transform.rotation.Set(cRotation.x, cRotation.y - rotationAmount * Time.deltaTime, cRotation.z, cRotation.w);
+			Debug.Log("Trying to turn");
+		}
+		else if(Input.GetKey(KeyCode.D)){
+			Quaternion cRotation = transform.rotation;
+			transform.rotation.Set(cRotation.x, cRotation.y + rotationAmount * Time.deltaTime, cRotation.z, cRotation.w);
+		}
+		
+		currentRotation = transform.rotation;
+
+
+	}
+
+	public float changeInRotation(){
+		return Quaternion.Angle(currentRotation, initialRotation);
+	}
+}
